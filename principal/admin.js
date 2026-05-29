51a3cf0cf49f564f0c43c5367ea4c0dd799753a3
Bruno Otavio <bruno.otavio.jm@outlook.com>
2026-05-29 19:59:55 -0300

    api

--- principal/admin.js
+++ principal/admin.js
@@ -3,12 +3,13 @@ import { getFirestore, collection, addDoc, doc, setDoc } from "https://www.gstat
 
 // COLOQUE SUAS CHAVES REAIS DO FIREBASE AQUI
 const firebaseConfig = {
-    apiKey: "SUA_API_KEY",
-    authDomain: "escatologiabiblica-c82c3.firebaseapp.com",
-    projectId: "escatologiabiblica-c82c3",
-    storageBucket: "escatologiabiblica-c82c3.firebasestorage.app",
-    messagingSenderId: "156849616608",
-    appId: "1:156849616608:web:a6eb47e0b00ca0ee824285"
+  apiKey: "AIzaSyBwPhRfJYSdBU_mmxyuCpCAkKQlKBhGHLI",
+  authDomain: "escatologiabiblica-c82c3.firebaseapp.com",
+  projectId: "escatologiabiblica-c82c3",
+  storageBucket: "escatologiabiblica-c82c3.firebasestorage.app",
+  messagingSenderId: "156849616608",
+  appId: "1:156849616608:web:a6eb47e0b00ca0ee824285",
+  measurementId: "G-ZQRZ7CPBPH"
 };
 
 const app = initializeApp(firebaseConfig);
